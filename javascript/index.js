6605e0e3544a4e4702a61a4fd45de3c59e0253b3
Yulie01 <yulietotoda01@gmail.com>
2026-03-09 13:10:07 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,30 @@
+function updateTime ()
+    //California
+{
+    let californiaElement = document.querySelector( "#california" );
+    if ( californiaElement )
+    {
+        let californiaDateElement = californiaElement.querySelector( ".date" );
+        let californiaTimeElement = californiaElement.querySelector( ".time" );
+        let californiaTime = moment().tz( "America/California" );
 
+        californiaDateElement.innerHTML = californiaTime.format( "MMMM Do YYYY" );
+        californiaElement.innerHTML = californiaTime.format( "h:mm:ss [<small>]A [</small]" );
+    }
+}
+    //Oslo
+{
+    let osloElement = document.querySelector( "#oslo" );
+    if ( osloElement )
+    {
+        let osloDateElement = osloElement.querySelector( "#.date" );
+        let osloTimeElement = osloElement.querySelector( "#time" );
+        let osloTime = moment().tz( "Europe/Oslo" );
+
+        osloDateElement.innerHTML = osloTime.format( "MMMM Do YYYY" );
+        osloTimeElement.innerHTML = osloTime.format( "h:mm:ss [<small>]A[</small>]" );
+    }
+}
+
+updateTime();
+setInterval(updateTime, 1000);
